6e36f65eb0db3f6bb01005c6fe5eeac5bedff8b3
stkrhafael@gmail.com <stkrhafael@gmail.com>
2022-08-02 14:10:55 -0400

    V 2.0

--- cmake-build-debug/CMakeFiles/Mercado_do_Bairro.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Mercado_do_Bairro.dir/DependInfo.cmake
@@ -8,21 +8,21 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/atuaPontos.c" "CMakeFiles/Mercado_do_Bairro.dir/atuaPontos.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/atuaPontos.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/atualClien.c" "CMakeFiles/Mercado_do_Bairro.dir/atualClien.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/atualClien.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/atualProd.c" "CMakeFiles/Mercado_do_Bairro.dir/atualProd.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/atualProd.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/cadNovClien.c" "CMakeFiles/Mercado_do_Bairro.dir/cadNovClien.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/cadNovClien.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/cadNovProd.c" "CMakeFiles/Mercado_do_Bairro.dir/cadNovProd.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/cadNovProd.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/estoqBaixo.c" "CMakeFiles/Mercado_do_Bairro.dir/estoqBaixo.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/estoqBaixo.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/listar1000.c" "CMakeFiles/Mercado_do_Bairro.dir/listar1000.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listar1000.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/listar18a25.c" "CMakeFiles/Mercado_do_Bairro.dir/listar18a25.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listar18a25.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/listarSet.c" "CMakeFiles/Mercado_do_Bairro.dir/listarSet.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listarSet.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/listarVendas.c" "CMakeFiles/Mercado_do_Bairro.dir/listarVendas.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listarVendas.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/programaPrincipal.c" "CMakeFiles/Mercado_do_Bairro.dir/programaPrincipal.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/programaPrincipal.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/subMenuClientes.c" "CMakeFiles/Mercado_do_Bairro.dir/subMenuClientes.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/subMenuClientes.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/subMenuProdutos.c" "CMakeFiles/Mercado_do_Bairro.dir/subMenuProdutos.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/subMenuProdutos.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/subMenuVendas.c" "CMakeFiles/Mercado_do_Bairro.dir/subMenuVendas.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/subMenuVendas.c.obj.d"
-  "C:/Users/rh4f4/OneDrive/Documentos/Mercado_do_Bairro/Sistema_de_Mercado/venda.c" "CMakeFiles/Mercado_do_Bairro.dir/venda.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/venda.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/atuaPontos.c" "CMakeFiles/Mercado_do_Bairro.dir/atuaPontos.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/atuaPontos.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/atualClien.c" "CMakeFiles/Mercado_do_Bairro.dir/atualClien.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/atualClien.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/atualProd.c" "CMakeFiles/Mercado_do_Bairro.dir/atualProd.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/atualProd.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/cadNovClien.c" "CMakeFiles/Mercado_do_Bairro.dir/cadNovClien.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/cadNovClien.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/cadNovProd.c" "CMakeFiles/Mercado_do_Bairro.dir/cadNovProd.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/cadNovProd.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/estoqBaixo.c" "CMakeFiles/Mercado_do_Bairro.dir/estoqBaixo.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/estoqBaixo.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/listar1000.c" "CMakeFiles/Mercado_do_Bairro.dir/listar1000.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listar1000.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/listar18a25.c" "CMakeFiles/Mercado_do_Bairro.dir/listar18a25.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listar18a25.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/listarSet.c" "CMakeFiles/Mercado_do_Bairro.dir/listarSet.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listarSet.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/listarVendas.c" "CMakeFiles/Mercado_do_Bairro.dir/listarVendas.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/listarVendas.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/programaPrincipal.c" "CMakeFiles/Mercado_do_Bairro.dir/programaPrincipal.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/programaPrincipal.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/subMenuClientes.c" "CMakeFiles/Mercado_do_Bairro.dir/subMenuClientes.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/subMenuClientes.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/subMenuProdutos.c" "CMakeFiles/Mercado_do_Bairro.dir/subMenuProdutos.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/subMenuProdutos.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/subMenuVendas.c" "CMakeFiles/Mercado_do_Bairro.dir/subMenuVendas.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/subMenuVendas.c.obj.d"
+  "C:/Users/rh4f4/OneDrive/Documentos/Sistema_de_Mercado/venda.c" "CMakeFiles/Mercado_do_Bairro.dir/venda.c.obj" "gcc" "CMakeFiles/Mercado_do_Bairro.dir/venda.c.obj.d"
   )
 
 # Targets to which this target links.
